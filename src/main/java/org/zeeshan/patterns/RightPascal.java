05074d7e5e09c502a4f0de0b3aaf40d3f5ada7ed
Zeeshan Ahmad <azeeshan867@gmail.com>
2026-02-02 23:56:04 +0530

    Update RightPascal.java

--- src/main/java/org/zeeshan/patterns/RightPascal.java
+++ src/main/java/org/zeeshan/patterns/RightPascal.java
@@ -19,6 +19,7 @@ public class RightPascal {
             }
             System.out.println();
         }
+
         for(int i=(n-1); i>=1; i--) {
             for(int j=1; j<=i; j++) {
                 System.out.print("*");
